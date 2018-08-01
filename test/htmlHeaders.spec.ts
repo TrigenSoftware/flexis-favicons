758a20158c17a8a03e26cc953871e6523325c61c
Dan Green <danon0404@gmail.com>
2018-08-01 17:08:37 +0700

    test, docs

---
+++ test/htmlHeaders.spec.ts
@@ -0,0 +1,136 @@
+import htmlHeaders from '../src/htmlHeaders';
+
+const {
+	favicon: faviconHeaders,
+	android: androidHeaders,
+	apple: appleHeaders,
+	appleStartup: appleStartupHeaders
+} = htmlHeaders;
+
+describe('htmlHeaders', () => {
+
+	describe('favicon', () => {
+
+		it('should return correct result', () => {
+
+			const headers = faviconHeaders({});
+
+			expect(headers.length).toBe(3);
+		});
+
+		it('should apply path', () => {
+
+			const path = 'icons';
+			const headers = faviconHeaders({ path });
+
+			headers.forEach(({
+				tagName,
+				href
+			}) => {
+
+				if (tagName === 'link') {
+					expect(href).toMatch(new RegExp(`^${path}`));
+				}
+			});
+		});
+	});
+
+	describe('android', () => {
+
+		it('should return correct result', () => {
+
+			const headers = androidHeaders({});
+
+			expect(headers.length).toBe(2);
+		});
+
+		it('should apply path', () => {
+
+			const path = 'icons';
+			const headers = androidHeaders({ path });
+
+			headers.forEach(({
+				tagName,
+				href
+			}) => {
+
+				if (tagName === 'link') {
+					expect(href).toMatch(new RegExp(`^${path}`));
+				}
+			});
+		});
+
+		it('should apply manifest', () => {
+
+			const manifest = {
+				name: 'App',
+				theme_color: '#fff'
+			};
+			const headers = androidHeaders({ manifest });
+
+			expect(headers.length).toBe(4);
+		});
+	});
+
+	describe('apple', () => {
+
+		it('should return correct result', () => {
+
+			const headers = appleHeaders({});
+
+			expect(headers.length).toBe(14);
+		});
+
+		it('should apply path', () => {
+
+			const path = 'icons';
+			const headers = appleHeaders({ path });
+
+			headers.forEach(({
+				tagName,
+				href
+			}) => {
+
+				if (tagName === 'link') {
+					expect(href).toMatch(new RegExp(`^${path}`));
+				}
+			});
+		});
+
+		it('should apply manifest', () => {
+
+			const manifest = {
+				name: 'App'
+			};
+			const headers = appleHeaders({ manifest });
+
+			expect(headers.length).toBe(15);
+		});
+	});
+
+	describe('appleStartup', () => {
+
+		it('should return correct result', () => {
+
+			const headers = appleStartupHeaders({});
+
+			expect(headers.length).toBe(10);
+		});
+
+		it('should apply path', () => {
+
+			const path = 'icons';
+			const headers = appleStartupHeaders({ path });
+
+			headers.forEach(({
+				tagName,
+				href
+			}) => {
+
+				if (tagName === 'link') {
+					expect(href).toMatch(new RegExp(`^${path}`));
+				}
+			});
+		});
+	});
+});
